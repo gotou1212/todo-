81e7e258242c77a2c112fa260e65944b52a02246
gotou1212 <meijiyou.20081212@gmail.com>
2025-01-28 14:50:10 +0000

    g

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todoList, setTodoList] = useState([
+    "todo1",
+    "todo2",
+    "todo3",
+    "todo4"
+  ]);
+
+  const handleClick = () => {
+    const newTodoList = todoList.map((todo) => {
+      return todo + "a";
+    })
+
+    // console.log(newTodoList);
+    setTodoList(newTodoList);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className='todoapp'>
+      <div className='todoapp-title'>TODOアプリ</div>
+
+      <div className='todoapp-add'>
+        <input type="text" className='todoapp-input__add' />
+        <button
+          className='todoapp-button__add'
+          onClick={(e) => {
+            handleClick();
+          }}
+        >
+          追加
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <ul className=''>
+        {todoList?.map(todo => {
+          return (
+            <div key={todo}>
+              {todo}
+            </div>
+          )
+        })}
+      </ul>
+
+      <div>
+        <input type="text" className='todoapp-input__search' />
+      </div>
+
+    </div>
   )
 }
 
 export default App
+
